c65b13eaf3735a46808457b3b59e5f81d1866267
Elaf Abdullah <ealh0002@student.monash.edu>
2020-10-24 21:16:09 +0800

    edit project navigation update

--- editProject.php
+++ editProject.php
@@ -31,7 +31,7 @@
       <div class="mdl-layout-spacer"></div>
       <!-- Navigation -->
       <nav class="mdl-navigation">
-        <a class="mdl-navigation__link" id="logout" href="">Log out</a>
+        <a class="mdl-navigation__link" href="script/logout.php">Log out</a>
         <div class="mdl-tooltip" data-mdl-for="logout">
         Log out
         </div>
